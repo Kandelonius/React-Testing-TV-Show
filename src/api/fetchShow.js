634b925a0899aa804a1edaadf659db6af0803fd8
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-18 13:03:28 -0700

    have the page rendering again.

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const url = "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes";
 export const fetchShow = () => {
-    return axios.get
-      .then(res => return res) // or res.data, however you want to set that up
-  }
+    return axios.get(url)
+        .then(res => {
+            return res;
+        })
+        .catch(err => {
+            console.error("error fetching data from api, err: ", err.message);
+            return err;
+        }); // or res.data, however you want to set that up
+}
